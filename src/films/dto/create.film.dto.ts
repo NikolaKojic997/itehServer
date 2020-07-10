dce4563869627bfc082249366c010c82c16b983a
nikolakojic997 <volimdahodam@gmail.com>
2020-07-10 17:00:42 +0200

    Added films controller with initial endpoints

---
+++ src/films/dto/create.film.dto.ts
@@ -0,0 +1,7 @@
+export class CreateFilmDto{
+    readonly FilmId: number;
+    readonly ImeFilma: string;
+    readonly GodinaProizvodnje: number;
+    readonly Trajanje: number;
+    readonly Produkcija: string;
+}
---
+++ src/films/films.controller.ts
@@ -0,0 +1,31 @@
+import {Controller, Get, Post, Put, Delete, Body, Param} from '@nestjs/common';
+import {CreateFilmDto} from "./dto/create.film.dto";
+
+@Controller('films')
+export class FilmsController {
+    @Get()
+    findAll() : string{
+        return "Get all films";
+    }
+
+    @Get(':id')
+    findOne(@Param() param): string{
+        return `film ${param.id}`;
+    }
+
+    @Post()
+    create(@Body() film: CreateFilmDto): string{
+        return `name: ${film.ImeFilma}, godinaProizvodnje: ${film.GodinaProizvodnje}`;
+    }
+
+    @Delete(':id')
+    remove(@Param() param): string{
+        return `deleting film ${param.id}`;
+    }
+
+    @Put(':id')
+    update(@Body() film: CreateFilmDto, @Param() param ):string{
+        return `Updating film with id ${param.id}`
+    }
+
+}
