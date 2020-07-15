64602a171c7e41d38e40b1aa4662532ff80c85ab
nikolakojic997 <volimdahodam@gmail.com>
2020-07-15 14:25:35 +0200

    All routes added.

--- src/grades/grades.controller.ts
+++ src/grades/grades.controller.ts
@@ -1,6 +1,7 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
 import {GradesService} from "./grades.service";
 import {Ocena} from "../entity/Ocena.entity";
+import {DeleteResult, UpdateResult} from "typeorm";
 
 @Controller('grades')
 export class GradesController {
@@ -11,4 +12,23 @@ export class GradesController {
         return this.gradesService.findAll();
     }
 
+    @Get(':id')
+    findOne(@Param() param): Promise<Ocena>{
+        return this.gradesService.findOne(param.id);
+    }
+
+    @Post()
+    create(@Body() ocena: Ocena):  Promise<Ocena>{
+        return this.gradesService.create(ocena);
+    }
+
+    @Delete(':id')
+    remove(@Param() param): Promise<DeleteResult>{
+        return this.gradesService.remove(param.id);
+    }
+
+    @Put(':id')
+    update(@Body() ocena: Ocena, @Param() param ):Promise<UpdateResult>{
+        return this.gradesService.update(ocena,param.id);
+    }
 }
--- src/grades/grades.service.ts
+++ src/grades/grades.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import {InjectRepository} from "@nestjs/typeorm";
-import {Film} from "../entity/Film.entity";
-import {Repository} from "typeorm";
+import {DeleteResult, Repository, UpdateResult} from "typeorm";
 import {Ocena} from "../entity/Ocena.entity";
 
 @Injectable()
@@ -12,5 +11,21 @@ export class GradesService {
     ) {}
     async findAll() : Promise<Ocena[]> {
         return this.gradesRepository.find({ relations: ["korisnik", "film"] });
+    }
+
+    async findOne(id: number): Promise<Ocena> {
+        return this.gradesRepository.findOne(id, { relations: ["korisnik", "film"] })
+    }
+
+    async create(ocena:Ocena) : Promise<Ocena>{
+        return this.gradesRepository.save(ocena, { });
+    }
+
+    async remove(id:number) : Promise<DeleteResult>{
+        return this.gradesRepository.delete(id);
+    }
+
+    async update(ocena: Ocena, id: any): Promise<UpdateResult> {
+        return this.gradesRepository.update(id,ocena);
     }
 }
